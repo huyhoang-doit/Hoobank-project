af7558281cf86378f253d6aab3fb88034ede3757
Le_Van_Huy_Hoang <lvhhoangg171@gmail.com>
2024-04-20 20:43:18 +0700

    Add business section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const Button = () => {
-  return (
-    <div>
-
-    </div>
-  )
-}
+const Button = ({ styles }) => (
+  <button
+    type='button'
+    className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px]
+    text-primary outline-none ${styles}`}
+  >
+    Get Started
+  </button>
+)
 
 export default Button
