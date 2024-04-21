0c72a9f83f3c64931df2310e2e67dbb06c8d26b1
Le_Van_Huy_Hoang <lvhhoangg171@gmail.com>
2024-04-21 07:53:19 +0700

    Add billing/ card deal section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ styles }) => (
   <button
     type='button'
     className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px]
-    text-primary outline-none ${styles}`}
+    text-primary outline-none rounded-[10px] ${styles}`}
   >
     Get Started
   </button>
